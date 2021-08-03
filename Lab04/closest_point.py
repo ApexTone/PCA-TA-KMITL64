bd69933d93ab83a45d8a4f8922c469b073dfadab
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2021-08-03 20:40:50 +0700

    Lab04 - Running Time: Fix Point class

--- Lab04/closest_point.py
+++ Lab04/closest_point.py
@@ -3,8 +3,17 @@ class Point:
         self.x = x
         self.y = y
 
-    def distance_from_origin(self):
-        return (self.x**2 + self.y**2)**0.5
+    # IMO: get_x() and get_y() are redundant, I'll skip it
+    def __repr__(self):
+        return f"({self.x},{self.y})"  # f-string
+
+    def __str__(self):
+        return "(%s, %s)" % (self.x, self.y)
+
+    def distance(self, other):
+        diff_x = self.x-other.x
+        diff_y = self.y-other.y
+        return (diff_x**2 + diff_y**2)**0.5
 
 
 def find_closest(points):
