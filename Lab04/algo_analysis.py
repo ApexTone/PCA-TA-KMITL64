f2b30cb2ea2a72a071471f1a90b65200208d223c
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2021-08-04 14:21:09 +0700

    Lab04 - Running Time: Implement m01-m12

--- Lab04/algo_analysis.py
+++ Lab04/algo_analysis.py
@@ -1,54 +1,155 @@
-# m1-m12 pseudocode in lab file
-def m1():
-    pass
+import matplotlib.pyplot as plt
+import time
 
 
-def m2():
-    pass
+def m1(n):  # O(n)
+    # don't use sum as variable name (same as sum() function)
+    rounds = 0
+    my_sum = 0
+    while rounds < n:
+        my_sum += 1
+        rounds += 1
+    return my_sum
 
 
-def m3():
-    pass
+def m2(n):  # O(n)
+    rounds = 0
+    my_sum = 0
+    while rounds < n:
+        my_sum += 1
+        rounds += 2
+    return my_sum
 
 
-def m4():
-    pass
+def m3(n):  # O(n^2)
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < n:
+            my_sum += 1
+            round2s += 1
+        round1s += 1
+    return my_sum
 
 
-def m5():
-    pass
+def m4(n):  # O(n^2)
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < n:
+            my_sum += 1
+            round2s += 10
+        round1s += 20
+    return my_sum
 
 
-def m6():
-    pass
+def m5(n):  # O(n)
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        my_sum += 1
+        round1s += 1
+    round2s = 0
+    while round2s < n:
+        my_sum += 1
+        round2s += 1
+    return my_sum
 
 
-def m7():
-    pass
+def m6(n):  # O(n^2)?
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < n*n:
+            my_sum += 1
+            round2s += 1
+        round1s += 1
+    return my_sum
 
 
-def m8():
-    pass
+def m7(n):  # O(n^2)?
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < round1s:
+            my_sum += 1
+            round2s += 1
+        round1s += 1
+    return my_sum
 
 
-def m9():
-    pass
+def m8(n):  # O(n^2)?
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < round1s*100:
+            my_sum += 1
+            round2s += 1
+        round1s += 1
+    return my_sum
 
 
-def m10():
-    pass
+def m9(n):  # O(n)?
+    round1s = 0
+    my_sum = 0
+    while round1s < n:
+        round2s = 0
+        while round2s < n * n:
+            round3s = 0
+            while round3s < round2s:
+                my_sum += 1
+                round3s += 1
+            round2s += 1
+        round1s += 1
+    return my_sum
 
 
-def m11():
-    pass
+def m10(n):  # O(log n)
+    rounds = 1
+    my_sum = 0
+    while rounds < n:
+        my_sum += 1
+        rounds *= 2
+    return my_sum
 
 
-def m12():
-    pass
+def m11(n):  # O(log n)
+    i = n
+    my_sum = 0
+    while i > 0:
+        my_sum += 1
+        i /= 2.0
+    return my_sum
+
+
+def m12(n):  # O(log10 n)?
+    rounds = 1
+    my_sum = 0
+    while rounds < n:
+        my_sum += 1
+        rounds *= 10
+    return my_sum
 
 
 def main():
-    pass
+    n = 100
+    m1(n)
+    m2(n)
+    m3(n)
+    m4(n)
+    m5(n)
+    m6(n)
+    m7(n)
+    m8(n)
+    m9(n)
+    m10(n)
+    m11(n)
+    m12(n)
 
 
 if __name__ == '__main__':
