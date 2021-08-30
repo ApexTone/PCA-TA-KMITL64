93bb9bdd1c8beeddd6e019808980a922afb25a1f
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2021-08-30 10:26:55 +0700

    Lab08 - Prelab

--- Lab08/prelab.py
+++ Lab08/prelab.py
@@ -15,14 +15,29 @@ def mystery1_iter(lst):
 
 
 def multiply_rec(a, b):
-    pass
+    if b == 1:
+        return a
+    return a + multiply_rec(a, b-1)
 
 
 def palindrome_rec(line):
-    pass
+    def _palindrome_rec(first, last):
+        if first > last:
+            return True
+        if line[first] != line[last]:
+            return False
+        return _palindrome_rec(first+1, last-1)
+    return _palindrome_rec(0, len(line)-1)
 
 
 def main():
-    lst = [66, 999, 123, 85, 12, 33, 59, 85123, 456, 789]
-    print(mystery1_iter(lst))
-    print(mystery1_rec(lst))
+    # lst = [66, 999, 123, 85, 12, 33, 59, 85123, 456, 789]
+    # print(mystery1_iter(lst))
+    # print(mystery1_rec(lst))
+    # print(multiply_rec(50, 3))
+    print(palindrome_rec('1221'))
+
+
+if __name__ == '__main__':
+    main()
+
